4ead42d167147ecd427cd54279e8448af7af0c7c
allangandelman <47567620+allangandelman@users.noreply.github.com>
2019-12-16 08:45:23 -0200

    queci de usar -m de novo

--- Scope-Test.ps1
+++
@@ -1,9 +0,0 @@
-﻿$test = "Ambito Script"
-function MyFunction ()
-{
-    $test = "Ambito Local"
-    Get-Variable -Name test -Scope 0
-    Get-Variable -Name test -Scope 1
-    Get-Variable -Name test -Scope 2
-}
-MyFunction
