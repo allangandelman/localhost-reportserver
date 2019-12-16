b5e3ed64692425a54766186a8b57218da60a460f
allangandelman <47567620+allangandelman@users.noreply.github.com>
2019-12-16 08:12:41 -0200

    commit 3

---
+++ Scope-Test.ps1
@@ -0,0 +1,9 @@
+﻿$test = "Ambito Script"
+function MyFunction ()
+{
+    $test = "Ambito Local"
+    Get-Variable -Name test -Scope 0
+    Get-Variable -Name test -Scope 1
+    Get-Variable -Name test -Scope 2
+}
+MyFunction
